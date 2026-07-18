763802ed4337a0b8b7c5cdcd4cab31358810059c
Osimxon <osimhondadaxanov2013@gmail.com>
2026-07-18 23:28:02 +0500

    fixed plus and minus

--- js/course.js
+++ js/course.js
@@ -7,7 +7,11 @@ plus.forEach((plyus, index) => {
 
     if (sbtl.style.display === "block") {
       sbtl.style.display = "none";
+      plyus.innerHTML = plyus.innerHTML.replace("-", "+");
     } else {
+      plyus.innerHTML = plyus.innerHTML.replace(
+        "+", "-"
+      );
       sbtl.style.display = "block";
       sbtl.style.width = "560px";
       sbtl.style.height = "104px";
@@ -18,8 +22,8 @@ plus.forEach((plyus, index) => {
       sbtl.style.color = "#424551";
       sbtl.style.padding = "20px 1px";
       sbtl.style.fontFamily = "'Lato', sans-serif";
-      sbtl.style.marginBottom = "50px"
-      sbtl.style.marginLeft = "40px"
+      sbtl.style.marginBottom = "50px";
+      sbtl.style.marginLeft = "40px";
     }
   });
 });
