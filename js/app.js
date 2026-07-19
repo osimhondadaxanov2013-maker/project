8ce68f92e7d751f1f1d50828d8f60102b9f242c5
Osimxon <osimhondadaxanov2013@gmail.com>
2026-07-19 22:41:04 +0500

    finished 1 page

--- js/app.js
+++ js/app.js
@@ -28,3 +28,27 @@ async function getWorkers() {
   });
 }
 getWorkers();
+
+const API_URL_SECOND = "http://localhost:3000/team";
+
+const teamContainer = document.getElementById("team__container");
+
+async function getTeam() {
+  let resTeam = await fetch(API_URL_SECOND);
+  let dataTeam = await resTeam.json();
+  console.log(dataTeam);
+
+  teamContainer.innerHTML = "";
+  dataTeam.forEach((member) => {
+    teamContainer.innerHTML += `
+      <div class="team__container--box">
+        <div class="container__box--img2">
+          <img class="box__img--img" src="${member.img}" alt="" />
+        </div>
+        <h2 class="container__box--name">${member.name}</h2>
+        <p class="container__box--job" >${member.job}</p>
+      </div>
+    `;
+  });
+}
+getTeam();
