c8f50baac10aee6475a97e0d84da8a1a90656078
Osimxon <osimhondadaxanov2013@gmail.com>
2026-07-24 16:03:16 +0500

    finished

--- js/event.js
+++ js/event.js
@@ -1,27 +1,18 @@
-const boxes = document.querySelectorAll("#box");
+const rows = document.querySelectorAll(".grid__list--row");
 const gridBtn = document.querySelector(".events__grid--table");
 const listBtn = document.querySelector(".events__grid--list");
-const table = document.querySelector("#table");
-const grids = document.querySelector('.grids')
+const grids = document.querySelector(".grids");
 
 gridBtn.addEventListener("click", () => {
-  boxes.forEach((box) => {
-    box.classList.remove("grid");
-    box.classList.add("list");
-    listBtn.style.color = "#000";
-    gridBtn.style.color = "#ff3f3a";
-    grids.classList.remove('grids')
-    grids.classList.add('list')
-  });
+  grids.classList.remove("card-view");
+  rows.forEach((row) => row.classList.remove("card-row"));
+  listBtn.style.color = "#000";
+  gridBtn.style.color = "#ff3f3a";
 });
 
 listBtn.addEventListener("click", () => {
-  boxes.forEach((box) => {
-    box.classList.remove("list");
-    box.classList.add("grid");
-    listBtn.style.color = "#ff3f3a";
-    gridBtn.style.color = "#000";
-    grids.classList.remove('list')
-    grids.classList.add('grids')
-  });
+  grids.classList.add("card-view");
+  rows.forEach((row) => row.classList.add("card-row"));
+  listBtn.style.color = "#ff3f3a";
+  gridBtn.style.color = "#000";
 });
