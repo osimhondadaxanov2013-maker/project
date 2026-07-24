9f79f0c5467eb10ec6dfb2164a1308455662775a
ejasur111-wq <ejasur111@gmail.com>
2026-07-24 14:21:16 +0500

    Yangilandi

--- js/contact.js
+++ js/contact.js
@@ -1,26 +1,62 @@
-const API_URL = "http://localhost:3000/workers";
+const API_URL = "http://localhost:3000/workerTwo";
 
 const container = document.getElementById("course-container");
-console.log(container);
 
 async function getWorkers() {
-  let res = await fetch(API_URL);
-  let data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(API_URL);
+    const data = await res.json();
 
-  container.innerHTML = "";
-  data.forEach((worker) => {
-    container.innerHTML += `
-          <div className="course__container--box">
-            <img src="${worker.img}" alt="" />
-            <div class="box__text--job">${worker.job}</div>
-            <h2>${worker.title}</h2>
-            <div class="box__text--price">
-            <p class="text__price--price">${worker.price}</p>
-            <span class="text__price--who">${worker.person}</span>
+    container.innerHTML = "";
+
+    data.forEach((worker) => {
+      container.innerHTML += `
+        <div class="course-card">
+          <div class="course-image">
+            <img src="${worker.img}" alt="${worker.title}">
           </div>
+
+          <div class="course-body">
+            <span class="course-category ${worker.class}">
+              ${worker.job}
+            </span>
+
+            <h3 class="course-title">
+              ${worker.title}
+            </h3>
+
+            <div class="course-info">
+              <span class="course-price">${worker.price}</span>
+              <span class="course-author">| ${worker.person}</span>
+            </div>
           </div>
-        `;
+        </div>
+      `;
+    });
+  } catch (error) {
+    console.log("Xatolik:", error);
+  }
+}
+
+getWorkers();
+
+const teamContainer = document.getElementById("team__container");
+
+async function getTeam() {
+  let resTeam = await fetch(API_URL_SECOND);
+  let dataTeam = await resTeam.json();
+  console.log(dataTeam);
+
+  teamContainer.innerHTML = "";
+  dataTeam.forEach((member) => {
+    teamContainer.innerHTML += `
+      <div class="team__container--box">
+        <div class="container__box--img2">
+          <img class="box__img--img" src="${member.img}" alt="" />
+        </div>
+        <h2 class="container__box--name">${member.name}</h2>
+        <p class="container__box--job" >${member.job}</p>
+      </div>
+    `;
   });
 }
-getWorkers()
